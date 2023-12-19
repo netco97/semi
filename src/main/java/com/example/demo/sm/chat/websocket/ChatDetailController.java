14ef50e058c60760ad64eba945c3bd8dacdc1293
Sangmin <101931428+netco97@users.noreply.github.com>
2023-12-19 12:52:12 +0900

    websocket 추가

--- src/main/java/com/example/demo/sm/chat/websocket/ChatDetailController.java
+++ src/main/java/com/example/demo/sm/chat/websocket/ChatDetailController.java
@@ -1,16 +1,22 @@
 package com.example.demo.sm.chat.websocket;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatDetailController {
+	private final SimpMessageSendingOperations messagingTemplate;
 	
-	@GetMapping("/room_detail/{roomId}")
-	public String chat_detail(@PathVariable String roomId) {
+	@MessageMapping("/chat")
+	public void send(ChatMessageDTO chatMessageDTO) {
+		System.out.println("sender 확인" + chatMessageDTO.getSender());
+		System.out.println("message 확인 " + chatMessageDTO.getMessage());
+		System.out.println("roomId 확인" + chatMessageDTO.getRoomId());
 		
-		
-		return "sm/room_detail";
+		messagingTemplate.convertAndSend("/sub/topic/"+ chatMessageDTO.getRoomId(), chatMessageDTO);
 	}
 }
--- src/main/resources/static/js/roomset.js
+++ src/main/resources/static/js/roomset.js
@@ -1,52 +1,106 @@
+var stompClient = null;
+let sessionId;
+let followerId;
+let roomId;
+
 function createChatroom() {
-    // 사용자가 입력한 데이터 가져오기
-    var sessionId = document.getElementById("session_id").value;
-    var followerId = document.getElementById("follower_id").value;
+    sessionId = document.getElementById("session_id").value;
+    followerId = document.getElementById("follower_id").value;
 
-	    var data = {
-	    session_id: sessionId,
-	    follower_id: followerId
-	};
+    var data = {
+        session_id: sessionId,
+        follower_id: followerId
+    };
 
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "connect_chatroom", true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	
-	xhr.onreadystatechange = function() {
-	    if (xhr.readyState === 4) {
-	        if (xhr.status === 200) {
-				var responseData = JSON.parse(xhr.response);
-				const roomId = responseData.room_id;
-				openModal(roomId);
-				//location.href = "room_detail/" +responseData.roomId;
-	            console.log(roomId);
-	        } else {
-	            console.error("통신 실패: " + xhr.status);
-	        }
-	    }
-	};
-	
-	xhr.onerror = function() {
-	    console.error("네트워크 오류로 통신에 실패했습니다.");
-	};
-	
-	xhr.send(JSON.stringify(data));
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "connect_chatroom", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var responseData = JSON.parse(xhr.response);
+                roomId = responseData.room_id;
+                openModal(roomId);
+
+                // WebSocket 연결
+                connectWebSocket(roomId);
+
+                console.log(roomId);
+            } else {
+                console.error("Communication failed: " + xhr.status);
+            }
+        }
+    };
+
+    xhr.onerror = function() {
+        console.error("Network error occurred.");
+    };
+
+    xhr.send(JSON.stringify(data));
 }
 
-// 모달 창 열기
+function connectWebSocket(roomId) {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame) {
+        console.log('Connected to WebSocket: ' + frame);
+
+        stompClient.subscribe('/sub/topic/' + roomId, function (response) {
+            var message = JSON.parse(response.body);
+            console.log("Message: " + message);
+            displayMessage(message);
+        });
+    });
+}
+
 function openModal(roomId) {
-  var modal = document.getElementById("myModal");
-  var modalContent = document.getElementById("modalContent");
+    var modal = document.getElementById("myModal");
+    var websocketResult = document.getElementById("websocketResult");
 
-  // 모달 창에 내용 추가
-  modalContent.innerText = "Room ID: " + roomId;
-
-  // 모달 창 표시
-  modal.style.display = "block";
+    websocketResult.innerText = "WebSocket Result: " + roomId;
+    modal.style.display = "block";
 }
 
-// 모달 창 닫기
 function closeModal() {
-  var modal = document.getElementById("myModal");
-  modal.style.display = "none";
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+
+    // WebSocket 연결 해제
+    disconnectWebSocket();
+}
+
+function disconnectWebSocket() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+        console.log("Disconnected from WebSocket");
+    }
+}
+
+function displayMessage(message) {
+    var messageArea = document.getElementById("messageArea");
+    var messageElement = document.createElement("div");
+    messageElement.innerText = message.sender + " : " + message.message;
+    messageArea.appendChild(messageElement);
+
+    // Scroll to the bottom
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+// Enter key sends the message
+document.getElementById("chatInput").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+    var chatInput = document.getElementById("chatInput");
+    var message = chatInput.value.trim();
+
+    if (message !== "") {
+        stompClient.send("/pub/chat", {}, JSON.stringify({'sender' : sessionId, 'message': message , 'roomId' : roomId}));
+        chatInput.value = "";
+    }
 }
