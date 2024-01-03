1fd8dca05cd1c74c9e1d6914a6d7e97294e5c420
(55555ta) <stiker3615@gmail.com>
2024-01-03 14:48:36 +0900

    메인 페이지 만드는중

--- src/main/java/com/example/demo/wk/HomeC.java
+++ src/main/java/com/example/demo/wk/HomeC.java
@@ -4,21 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.th.music_comment.MusicCommentService;
+import com.example.demo.th.musicsearch.MusicService;
+import com.example.demo.th.musicsearch.TagsDTO;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeC {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        // 중간 페이지 내용을 설정
-        model.addAttribute("content", "wk/home");
-        return "wk/index";
-    }
-    
+	private final MusicService musicService;
+
+	@GetMapping("/")
+	public String home(Model model) {
+		// 중간 페이지 내용을 설정
+		TagsDTO tags = (TagsDTO) musicService.getTags();
+		model.addAttribute("content", "wk/home");
+		model.addAttribute("tags", tags);
+
+		return "wk/index";
+	}
+
 	@GetMapping("/login")
 	public String login(Model model) {
-		
+
 		model.addAttribute("content", "dw_view/04_Login");
 		return "wk/index";
 	}
-		
+
 }
---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,81 @@
+
+
+var c = document.getElementById("cSound");
+cSound.playbackRate = 4;
+
+function cPlay() {
+	c.play();
+	clickOption('genre')
+}
+
+var cH = document.getElementById("cHSound");
+cHSound.playbackRate = 4;
+
+function cHPlay() {
+	cH.play();
+}
+var d = document.getElementById("dSound");
+dSound.playbackRate = 4;
+
+function dPlay() {
+	d.play();
+	clickOption('mood')
+}
+var dH = document.getElementById("dHSound");
+dHSound.playbackRate = 4;
+
+function dHPlay() {
+	dH.play();
+}
+var e = document.getElementById("eSound");
+eSound.playbackRate = 4;
+
+function ePlay() {
+	e.play();
+	clickOption('instrument')
+}
+var f = document.getElementById("fSound");
+fSound.playbackRate = 4;
+
+function fPlay() {
+	f.play();
+//	아티스트
+}
+var fH = document.getElementById("fHSound");
+fHSound.playbackRate = 4;
+
+function fHPlay() {
+	fH.play();
+}
+var g = document.getElementById("gSound");
+gSound.playbackRate = 4;
+
+function gPlay() {
+	g.play();
+//	인기순
+}
+var gH = document.getElementById("gHSound");
+gHSound.playbackRate = 4;
+
+function gHPlay() {
+	gH.play();
+}
+var a = document.getElementById("aSound");
+aSound.playbackRate = 4;
+
+function aPlay() {
+	a.play();
+//	최신순
+}
+var aH = document.getElementById("aHSound");
+aHSound.playbackRate = 4;
+
+function aHPlay() {
+	aH.play();
+}
+var b = document.getElementById("bSound");
+bSound.playbackRate = 4;
+
+function bPlay() {
+	b.play();
+} 
