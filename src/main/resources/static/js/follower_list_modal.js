d34384a5ca76040c00dd5327fdbd5770aeca771c
k_dy <ehdrmsrla123@gmail.com>
2024-01-04 03:23:07 +0900

    css수정중

--- src/main/resources/static/js/follower_list_modal.js
+++ src/main/resources/static/js/follower_list_modal.js
@@ -2,48 +2,49 @@
 
 // 모달창 닫기
 function closeFollowerListModal() {
-    $('#followerListModal').hide();
+	$('#followerListModal').hide();
 }
 
 // 모달창 열기
 function showFollowerListModal(followerList) {
-    $('#followerListModalContent').html(buildFollowerListHTML(followerList));
-    $('#followerListModal').show();
+	$('#followerListModalContent').html(buildFollowerListHTML(followerList));
+	$('#followerListModal').show();
 }
 
 // 팔로우 리스트 HTML 생성
 function buildFollowerListHTML(followerList) {
-    var html = '<ul>';
-    
-    console.log(followerList)
+	var html = '<ul>';
 
-    for (var i = 0; i < followerList.length; i++) {
-        html += '<div onclick="gotodetail(' + followerList[i].targetUserId + ');">' + followerList[i].composer_name +
-                '<img src="/images/profile/'+ followerList[i].composer_img+ '" style="max-width: 100px; max-height: 100px;">' +
-                '</div>';
-        console.log(JSON.stringify(followerList[i]))
-    }
-    html += '</ul>';
-    return html;
+	console.log(followerList)
+
+	for (var i = 0; i < followerList.length; i++) {
+		html += '<div onclick="gotodetail(' + "" + followerList[i].targetUserId + ');">' +
+		'<img src="/images/profile/' + followerList[i].composer_img + '" style="width: 100px; height: 100px;">' +
+		'<p>'+'</p>' + followerList[i].composer_name + 
+			'</div>';
+		console.log(JSON.stringify(followerList[i]))
+	}
+	html += '</ul>';
+	return html;
 }
 
-function gotodetail(phone_follower){
-	
+function gotodetail(phone_follower) {
+
 	console.log(phone_follower)
-	
+
 	const detailPageUrl = "/artist_detail";
-	
+
 	const form = $('<form>', {
-		'action' : detailPageUrl,
-		'method' : 'post',
-		'style' : 'display:none;'
+		'action': detailPageUrl,
+		'method': 'post',
+		'style': 'display:none;'
 	});
-	
+
 	$('<input>').attr({
-		'type' : 'hidden',
-		'name' : 'userFullPhoneNumber',
-		'value' : '0'+phone_follower
+		'type': 'hidden',
+		'name': 'userFullPhoneNumber',
+		'value': "0" + phone_follower
 	}).appendTo(form);
-	
+
 	form.appendTo('body').submit();
 }
